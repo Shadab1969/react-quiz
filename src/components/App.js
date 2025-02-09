38f9f9edbfc75b86822273dbc8aedc22efd647a8
shadab1969 <shadabanwar2050@gmail.com>
2025-02-10 01:37:42 +0530

    included api for question using backend

--- src/components/App.js
+++ src/components/App.js
@@ -103,10 +103,16 @@ export default function App() {
   );
 
   useEffect(function () {
-    fetch("http://localhost:8000/questions")
+    // fetch("http://localhost:8000/questions")
+    fetch("https://newapi-px7q.onrender.com")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
+
+    /* fetch(`${process.env.PUBLIC_URL}/question.json`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));*/
   }, []);
 
   return (
